882543110808201c3ac0bba5275a3f120343d0e4
Sérgio Filho <fragasergio23@gmail.com>
2022-05-22 02:05:06 -0300

    Implementando rotas

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const port = require('./routes')
+const express = require('express')
 
-module.exports = function(server) {
-    const protectApi = express.Router();
-    server.use("/api", protectApi);
+module.exports = function (server) {
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi);
 
-    server.use("/status", (req, res) =>
+    server.use('/status', (req, res) =>
         res.send(`BACKEND is runner.`)
     );
 
-    server.use(express.static(require("path").join(__dirname, "../public")))
+    const register = require('../api/register/registerService');
+    register.register(protectedApi, '/register');
+
+    server.use(express.static(require('path').join(__dirname, '../public')));
 }
